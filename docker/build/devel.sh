e8fe3dc01676fe754c966686deaaa64cbace4365
Jeremías <jrmsdev@gmail.com>
2021-02-13 21:20:28 -0300

    fix build img devel container name

--- docker/build/devel.sh
+++ docker/build/devel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec docker run -it --rm --name uwspkg-devel \
-	--hostname devel.uwspkg.local -u uws uwspkg/build $@
+exec docker run -it --rm --name uwspkg-build-devel \
+	--hostname build-devel.uwspkg.local -u uws uwspkg/build $@
