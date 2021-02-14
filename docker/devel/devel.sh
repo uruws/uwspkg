0506e6932df616e0c52f7f09b0e6501e56151371
Jeremías <jrmsdev@gmail.com>
2021-02-14 18:52:53 -0300

    docker devel mount local sources to work with

--- docker/devel/devel.sh
+++ docker/devel/devel.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 exec docker run -it --rm --name uwspkg-devel \
-	--hostname devel.uwspkg.local -u uws uwspkg/devel $@
+	--hostname devel.uwspkg.local \
+	-v ${PWD}:/home/uws/src/uwspkg \
+	-u uws uwspkg/devel $@
