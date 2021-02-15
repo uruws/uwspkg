f17e1c2636bad395e8786b6a347b3a5b34aef54d
Jeremías <jrmsdev@gmail.com>
2021-02-15 18:06:24 -0300

    make uwspkg bootstrap

--- base/pkg/utils/mkpkg.sh
+++ base/pkg/utils/mkpkg.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -eu
 
+export PATH=/uws/sbin:${PATH}
+
 mkdir -vp /home/uws/build
 build=$(mktemp -d /home/uws/build/pkg-XXXXXXXX)
 files=/home/uws/src
-dstdir=/home/uws/build
+dstfn=/home/uws/build/uwspkg-bootstrap.tgz
 
+oldwd=${PWD}
 cd /uws
 rm -vrf etc share include
 
@@ -14,4 +17,13 @@ install -v -m 0644 ${files}/etc/pkg.conf etc/
 
 cat ${files}/manifest >${build}/+MANIFEST
 
+cd ${build}
+pkg create -v -m . -r /uws
+pkg register -d -m .
+
+cd ${oldwd}
+rm -rf ${build}
+
+tar -C / -vczf ${dstfn} ./uws
+echo "${dstfn} done!"
 exit 0
