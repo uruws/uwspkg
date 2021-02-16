12bbd53f61aad0549eb02da1f2e4e07a2a78fcb7
Jeremías <jrmsdev@gmail.com>
2021-02-16 16:58:24 -0300

    check tar file extract and pkg bin stat

--- docker/check/devel.sh
+++ docker/check/devel.sh
@@ -2,6 +2,8 @@
 mkdir -vp ${PWD}/build
 exec docker run -it --rm --network none --name uwspkg-check-devel \
 	--hostname check-devel.uwspkg.local \
+	--add-host check-devel.uwspkg.local:127.10.0.1 \
 	-v ${PWD}/build:/home/uws/build \
 	-v ${PWD}/check:/home/uws/check \
+	--entrypoint /usr/local/bin/uws-login.sh \
 	-u uws uwspkg/check $@
--- docker/check/run.sh
+++ docker/check/run.sh
@@ -2,6 +2,7 @@
 mkdir -vp ${PWD}/build
 exec docker run --rm --network none --name uwspkg-check \
 	--hostname check.uwspkg.local \
+	--add-host check.uwspkg.local:127.10.0.1 \
 	-v ${PWD}/build:/home/uws/build \
 	-v ${PWD}/check:/home/uws/check \
 	-u uws uwspkg/check $@
