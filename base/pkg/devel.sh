3ce9332bd15c7a134ebaf384427bc5afecdeba09
Jeremías <jrmsdev@gmail.com>
2021-02-16 22:02:33 -0300

    20210217 version

--- base/pkg/devel.sh
+++ base/pkg/devel.sh
@@ -2,4 +2,5 @@
 exec docker run -it --rm --name uwspkg-pkg-devel \
 	--hostname pkg-devel.uwspkg.local \
 	--entrypoint /usr/local/bin/uws-login.sh \
+	-e UWSPKG_VERSION=$(cat VERSION) \
 	-u uws uwspkg/pkg $@
--- base/pkg/make.sh
+++ base/pkg/make.sh
@@ -3,4 +3,5 @@ mkdir -vp ${PWD}/build
 exec docker run -it --rm --name uwspkg-make \
 	--hostname make.uwspkg.local \
 	-v ${PWD}/build:/home/uws/build \
+	-e UWSPKG_VERSION=$(cat VERSION) \
 	-u uws uwspkg/pkg
