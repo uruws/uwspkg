1cab7006c70e66ea9013a98a84088e68327ee1ff
Jeremías <jrmsdev@gmail.com>
2021-03-25 01:47:55 -0300

    docker devel mkpkg.sh deps

--- docker/devel/devel.sh
+++ docker/devel/devel.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 exec docker run -it --rm --name uwspkg-devel \
 	--hostname devel.uwspkg.local \
-	-v ${PWD}/build:/home/uws/build \
 	-v ${PWD}/base/uwspkg/files:/home/uws/src:ro \
-	-v ${PWD}:/uws/src/uwspkg:ro \
+	-v ${PWD}:/opt/src/uwspkg \
 	-e UWSPKG_VERSION=$(cat VERSION) \
 	-u uws uwspkg/devel $@
