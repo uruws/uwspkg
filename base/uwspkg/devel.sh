dd45cdcf92e05c350f998d34248c7f03b7335821
Jeremías <jrmsdev@gmail.com>
2021-03-25 01:22:40 -0300

    uwspkg devel container mount build and source files dirs

--- base/uwspkg/devel.sh
+++ base/uwspkg/devel.sh
@@ -3,4 +3,6 @@ exec docker run -it --rm --name uwspkg-uwspkg-devel \
 	--hostname uwspkg-devel.uwspkg.local \
 	--entrypoint /usr/local/bin/uws-login.sh \
 	-e UWSPKG_VERSION=$(cat VERSION) \
+	-v ${PWD}/build:/home/uws/build \
+	-v ${PWD}/base/uwspkg/files:/home/uws/src:ro \
 	-u uws uwspkg/base:uwspkg $@
