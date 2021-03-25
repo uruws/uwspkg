14af464d447ca3652fd7e78a6cea95a544da3f97
Jeremías <jrmsdev@gmail.com>
2021-03-25 16:20:35 -0300

    go docker debug log level

--- go/docker/devel.sh
+++ go/docker/devel.sh
@@ -2,4 +2,6 @@
 exec docker run -it --rm --name uwspkg-golang \
 	--hostname golang.uwspkg.local \
 	-v ${PWD}/go:/go/src/uwspkg \
+	-e UWSPKG_LOG='debug' \
+	-e UWSPKG_LOG_COLORS='auto' \
 	-u uws uwspkg/golang $@
