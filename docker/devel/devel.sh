0355348ef6a1805c8440f4732e99b713e3ac0fdd
Jeremías <jrmsdev@gmail.com>
2021-03-25 01:33:40 -0300

    docker devel set UWSPKG_VERSION env var

--- docker/devel/devel.sh
+++ docker/devel/devel.sh
@@ -3,4 +3,5 @@ exec docker run -it --rm --name uwspkg-devel \
 	--hostname devel.uwspkg.local \
 	-v ${PWD}/build:/home/uws/build \
 	-v ${PWD}:/home/uws/src/uwspkg \
+	-e UWSPKG_VERSION=$(cat VERSION) \
 	-u uws uwspkg/devel $@
