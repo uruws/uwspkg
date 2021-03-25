d74a51813160bae941b61f0b9fc93d44cc5640cb
Jeremías <jrmsdev@gmail.com>
2021-03-25 01:38:50 -0300

    docker devel mount sources

--- docker/devel/devel.sh
+++ docker/devel/devel.sh
@@ -2,6 +2,7 @@
 exec docker run -it --rm --name uwspkg-devel \
 	--hostname devel.uwspkg.local \
 	-v ${PWD}/build:/home/uws/build \
-	-v ${PWD}:/home/uws/src/uwspkg:ro \
+	-v ${PWD}/base/uwspkg/files:/home/uws/src:ro \
+	-v ${PWD}:/uws/src/uwspkg:ro \
 	-e UWSPKG_VERSION=$(cat VERSION) \
 	-u uws uwspkg/devel $@
