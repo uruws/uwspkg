30ae3c44658dd19a37fea600a89485160ce866d0
Jeremías <jrmsdev@gmail.com>
2021-03-25 01:32:28 -0300

    docker devel mount build dir

--- docker/devel/devel.sh
+++ docker/devel/devel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 exec docker run -it --rm --name uwspkg-devel \
 	--hostname devel.uwspkg.local \
+	-v ${PWD}/build:/home/uws/build \
 	-v ${PWD}:/home/uws/src/uwspkg \
 	-u uws uwspkg/devel $@
