c9306554f4254665ac93564e849dc67f7990383c
Jeremías <jrmsdev@gmail.com>
2021-03-25 18:20:31 -0300

    uwspkg define package struct

--- go/uwspkg.go
+++ go/uwspkg.go
@@ -1,5 +1,13 @@
 // Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
 // See LICENSE file.
 
-// Package uwspkg.
+// Package uwspkg defines the package specs.
 package uwspkg
+
+type Package struct {
+	Name string `yaml:"name"`
+}
+
+func New() *Package {
+	return &Package{}
+}
---
+++ go/uwspkg_test.go
@@ -0,0 +1,11 @@
+// Copyright (c) Jeremías Casteglione <jeremias@talkingpts.org>
+// See LICENSE file.
+
+package uwspkg
+
+import (
+	"testing"
+)
+
+func TestPackage(t *testing.T) {
+}
