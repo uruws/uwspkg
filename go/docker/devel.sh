8b3b536363f0736fa544e63033aee0f5ce8064df
Jeremías <jrmsdev@gmail.com>
2021-03-26 00:32:32 -0300

    devel source package files

--- go/docker/devel.sh
+++ go/docker/devel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 exec docker run -it --rm --name uwspkg-golang \
 	--hostname golang.uwspkg.local \
+	-v ${PWD}/base:/usr/local/src/base:ro \
+	-v ${PWD}/devel:/usr/local/src/devel:ro \
 	-v ${PWD}/go:/go/src/uwspkg \
 	-e UWSPKG_LOG='debug' \
 	-e UWSPKG_LOG_COLORS='auto' \
