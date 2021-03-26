8407b052e82984ec9459d66ead9038be6bcb7936
Jeremías <jrmsdev@gmail.com>
2021-03-26 19:03:32 -0300

    schroot uwspkg-build profile

--- go/cmd/uwspkg-build-setup.sh
+++ go/cmd/uwspkg-build-setup.sh
@@ -5,6 +5,7 @@ REPO=http://deb.debian.org/debian
 SRV=/srv/uwspkg
 mkdir -vp ${SRV}
 debinst='debootstrap --variant=minbase'
+
 if ! test -d ${SRV}/chroot/debian-buster; then
 	${debinst} buster ${SRV}/chroot/debian-buster ${REPO}
 	oldwd=${PWD}
@@ -13,16 +14,22 @@ if ! test -d ${SRV}/chroot/debian-buster; then
 		./var/cache/apt/*cache.bin
 	cd ${oldwd}
 fi
-mkdir -vp ${SRV}/union/overlay ${SRV}/union/underlay
+
+mkdir -vp ${SRV}/build ${SRV}/union/overlay ${SRV}/union/underlay
+
 schsrc=./etc/schroot
 schdst=/etc/schroot
 schroot_conf=${schsrc}/chroot.d/uwspkg.conf
-PROFILES='default'
+PROFILES='default build'
+
 if test -s ${schroot_conf}; then
 	install -v -C -m 0644 ${schroot_conf} ${schdst}/chroot.d/uwspkg.conf
 	for prof in ${PROFILES}; do
-		mkdir -vp ${schdst}/uwspkg-${prof}
-		install -v -C -m 0644 ${schsrc}/uwspkg-${prof}/* ${schdst}/uwspkg-${prof}
+		dst="${schdst}/uwspkg-${prof}"
+		rm -rf ${dst}
+		install -m 0755 -d ${dst}
+		install -m 0644 ${schsrc}/uwspkg-${prof}/* ${dst}
 	done
 fi
+
 exit 0
