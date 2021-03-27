f76f8bfb70012c31c61eab851b04c1e87880d970
Jeremías <jrmsdev@gmail.com>
2021-03-27 03:19:27 -0300

    build test suite

--- go/build/build_test.go
+++ go/build/build_test.go
@@ -4,5 +4,42 @@
 package build
 
 import (
-	_ "uwspkg/_testing/tset"
+	"testing"
+
+	_ "uwspkg/_testing/setup"
+
+	"uwspkg/config"
+	"uwspkg/_testing/tset"
+
+	. "gopkg.in/check.v1"
 )
+
+func init() {
+	Suite(&TSuite{})
+}
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type TSuite struct {
+	cfg        *config.Main
+	mockRunner *tset.LibexecMockRunner
+}
+
+func (s *TSuite) SetUpTest(c *C) {
+	var err error
+	s.cfg, err = config.Load()
+	c.Assert(err, IsNil)
+	s.mockRunner = tset.NewLibexecMockRunner()
+	tset.LibexecRunner(s.mockRunner)
+}
+
+func (s *TSuite) TearDownTest(c *C) {
+	s.cfg = nil
+	s.mockRunner = nil
+	tset.LibexecDefaultRunner()
+}
+
+func (s *TSuite) TestEnvSetUp(c *C) {
+}
