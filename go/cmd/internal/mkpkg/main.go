e760632564bb136de6e819b98df439bd8c6546b3
Jeremías <jrmsdev@gmail.com>
2021-03-31 00:08:06 -0300

    mkpkg load source package manifest

--- go/cmd/internal/mkpkg/main.go
+++ go/cmd/internal/mkpkg/main.go
@@ -5,9 +5,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path"
+	"path/filepath"
 
 	"uwspkg/log"
+	"uwspkg/manifest"
 )
 
 func main() {
@@ -18,30 +22,35 @@ func main() {
 	if pkgorig == "" {
 		log.Error("UWSPKG_ORIGIN not set")
 	}
+	mfn := path.Join("/uwspkg/src", pkgorig, "manifest.yml")
+	m := manifest.New(pkgorig)
+	if err := m.Load(mfn); err != nil {
+		log.Fatal("%v", err)
+	}
 	log.Print("mkpkg end")
 }
 
-//~ func writeManifest(m *manifest.Config) error {
-	//~ fn := filepath.Join("/build", m.BuildSession, m.Package+".manifest")
-	//~ log.Debug("%s write manifest: %s", m.Session, fn)
-	//~ if _, err := os.Stat(fn); err == nil {
-		//~ return fmt.Errorf("%s: file already exists", fn)
-	//~ } else {
-		//~ if !os.IsNotExist(err) {
-			//~ return err
-		//~ }
-	//~ }
-	//~ fh, err := os.OpenFile(fn, os.O_WRONLY, 0640)
-	//~ if err != nil {
-		//~ return err
-	//~ }
-	//~ defer fh.Close()
-	//~ if n, err := fh.WriteString(m.String()); err != nil {
-		//~ return err
-	//~ } else {
-		//~ if n != len(m.String()) {
-			//~ return fmt.Errorf("write manifest: wrong number of bytes")
-		//~ }
-	//~ }
-	//~ return nil
-//~ }
+func writeManifest(m *manifest.Config) error {
+	fn := filepath.Join("/build", m.BuildSession, m.Package+".manifest")
+	log.Debug("%s write manifest: %s", m.Session, fn)
+	if _, err := os.Stat(fn); err == nil {
+		return fmt.Errorf("%s: file already exists", fn)
+	} else {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	}
+	fh, err := os.OpenFile(fn, os.O_WRONLY, 0640)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	if n, err := fh.WriteString(m.String()); err != nil {
+		return err
+	} else {
+		if n != len(m.String()) {
+			return fmt.Errorf("write manifest: wrong number of bytes")
+		}
+	}
+	return nil
+}
