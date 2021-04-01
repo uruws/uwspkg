be15dd06985a4e3bd5691c88f5d052993697e14d
Jeremías <jrmsdev@gmail.com>
2021-03-31 22:59:56 -0300

    schroot session begin/end

--- go/libexec/schroot.go
+++ go/libexec/schroot.go
@@ -10,6 +10,7 @@ type Chroot struct {
 	cmd  string
 	dir  string
 	user string
+	sess bool
 }
 
 func NewChroot(name string) *Chroot {
@@ -29,6 +30,14 @@ func (c *Chroot) User(u string) {
 	c.user = u
 }
 
+func (c *Chroot) SessionBegin() {
+	c.sess = true
+}
+
+func (c *Chroot) SessionEnd() {
+	c.sess = false
+}
+
 func (c *Chroot) Run(env *Env, cmd string, args ...string) error {
 	runargs := make([]string, 0)
 	runargs = append(runargs, "-p")
@@ -38,8 +47,14 @@ func (c *Chroot) Run(env *Env, cmd string, args ...string) error {
 		runargs = append(runargs, "-u")
 		runargs = append(runargs, c.user)
 	}
-	runargs = append(runargs, "-c")
-	runargs = append(runargs, c.name)
+	if c.sess {
+		runargs = append(runargs, "-r")
+		runargs = append(runargs, "-c")
+		runargs = append(runargs, c.name)
+	} else {
+		runargs = append(runargs, "-c")
+		runargs = append(runargs, c.name)
+	}
 	runargs = append(runargs, "--")
 	cmdpath := path.Join("/uwspkg/libexec", cmd)
 	runargs = append(runargs, cmdpath)
